01713bc75be7da2e1e9298c4ae9594076525af4e
Kirill Kuleshov <nstkuleshov1@gmail.com>
2019-07-16 00:27:03 +0300

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,16 @@
 
+//=include ../components/header-home/header-home.js
+//=include ../components/header-about/header-about.js
+//=include ../components/page-nav/page-nav.js
+//=include ../components/preview/preview.js
+//=include ../components/preview-slider/preview-slider.js
+//=include ../components/department/department.js
+//=include ../components/work/work.js
+//=include ../components/start/start.js
+//=include ../components/personas/personas.js
+//=include ../components/graph/graph.js
+//=include ../components/strategy/strategy.js
+//=include ../components/locator/locator.js
+//=include ../components/device/device.js
+//=include ../components/think/think.js
 //=include ../components/footer/footer.js
